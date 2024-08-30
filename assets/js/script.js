25ac1cc5fd297851b0a554fc521cd4170b03b463
esbonmutisyamaina <mainaesbon5@gmail.com>
2024-08-30 10:34:59 +0300

    On brach main make changes in the project desc and category make changes in the projets header and button-group make the necessary changes in the script.js make changes in the index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Actuarial Science Modelling", "Financial Mathematics", "Data Analytics", "Risk Management", "Predictive Analysis"],
+    strings: ["Actuarial Modelling", "Financial Mathematics", "Data Analytics", "Risk Management", "Predictive Analytics"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
