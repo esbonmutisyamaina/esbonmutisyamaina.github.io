4c7734bda8b39a51148823489176d4bc31813119
esbonmutisyamaina <mainaesbon5@gmail.com>
2025-07-11 12:15:00 +0300

    feat : change index.html feat : change script.js feat : change skills.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Actuarial Modelling", "Financial Mathematics", "Data Analytics", "Risk Management", "Predictive Analytics"],
+    strings: ["Actuarial Modelling", "Insurance & Underwriting", "Finance & Investment", "Data Analytics", "Risk Management", "Predictive Analytics"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
